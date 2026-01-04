b3ca2478fdc3266eb6e5ab89e7e527b65661d560
Mona <xiaomei.mona.he@gmail.com>
2026-01-03 18:34:23 -0700

    chore(db): update EF Core mappings and migrations for booking rules

---
+++ src/Alberta.ServiceDesk.EntityFrameworkCore/Migrations/20260104004243_AddBookingProperties.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alberta.ServiceDesk.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookingProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndTime",
+                table: "AppBookings",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "FacilityId",
+                table: "AppBookings",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Purpose",
+                table: "AppBookings",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "AppBookings",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "AppBookings",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppBookings_FacilityId",
+                table: "AppBookings",
+                column: "FacilityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppBookings_AppFacilities_FacilityId",
+                table: "AppBookings",
+                column: "FacilityId",
+                principalTable: "AppFacilities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppBookings_AppFacilities_FacilityId",
+                table: "AppBookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppBookings_FacilityId",
+                table: "AppBookings");
+
+            migrationBuilder.DropColumn(
+                name: "EndTime",
+                table: "AppBookings");
+
+            migrationBuilder.DropColumn(
+                name: "FacilityId",
+                table: "AppBookings");
+
+            migrationBuilder.DropColumn(
+                name: "Purpose",
+                table: "AppBookings");
+
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "AppBookings");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AppBookings");
+        }
+    }
+}
--- src/Alberta.ServiceDesk.EntityFrameworkCore/ServiceDeskModelBuilderExtensions.cs
+++ src/Alberta.ServiceDesk.EntityFrameworkCore/ServiceDeskModelBuilderExtensions.cs
@@ -1,10 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+using Alberta.ServiceDesk.Facilities;
+using Alberta.ServiceDesk.Bookings;
+using Alberta.ServiceDesk.Policies;
 
-namespace Alberta.ServiceDesk
+namespace Alberta.ServiceDesk.EntityFrameworkCore;
+
+public static class ServiceDeskModelBuilderExtensions
 {
-    internal class ServiceDeskModelBuilderExtensions
+    public static void ConfigureServiceDesk(this ModelBuilder builder)
     {
+        Check.NotNull(builder, nameof(builder));
+
+        builder.Entity<Facility>(b =>
+        {
+            b.ToTable("facilities");
+            b.ConfigureByConvention();
+            b.Property(x => x.Name).IsRequired().HasMaxLength(128);
+            b.Property(x => x.Type).HasMaxLength(64);
+            b.Property(x => x.OwnerUnit).HasMaxLength(64);
+            b.Property(x => x.Description).HasMaxLength(512);
+
+            b.HasIndex(x => new { x.OwnerUnit, x.Type });
+        });
+
+        builder.Entity<Booking>(b =>
+        {
+            b.ToTable("bookings");
+            b.ConfigureByConvention();
+            b.Property(x => x.Purpose).IsRequired().HasMaxLength(256);
+
+            b.HasIndex(x => new { x.FacilityId, x.StartTime });
+        });
+
+        builder.Entity<BookingApproval>(b =>
+        {
+            b.ToTable("booking_approvals");
+            b.ConfigureByConvention();
+        });
+
+        builder.Entity<PolicyRule>(b =>
+        {
+            b.ToTable("policy_rules");
+            b.ConfigureByConvention();
+        });
     }
 }
